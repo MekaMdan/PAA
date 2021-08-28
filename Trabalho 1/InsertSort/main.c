56a91644506ea8cca74579e2ed23054820a9090e
Mariana <m.alencar.v@outlook.com>
2021-08-28 01:34:04 -0300

    começo insert

---
+++ Trabalho 1/InsertSort/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int vezes,i; // casos de teste
+    int T[5];
+    scanf("%d",&vezes);
+    i = 0;
+    while(i<vezes){
+        i++;
+        printf("vezes = %d\n",i);
+    } 
+    return 0;
+}
